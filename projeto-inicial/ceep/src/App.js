fb90da7e22f734ed5cc342fd59edabc25dcb5c25
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2021-05-27 09:40:20 -0300

    Upgrade code

--- projeto-inicial/ceep/src/App.js
+++ projeto-inicial/ceep/src/App.js
@@ -1,7 +1,18 @@
-function App() {
-  return (
-    <h1>Meu app react</h1>
-  );
+import React,{ Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas.jsx";
+import FormularioCadastro from "./components/FormularioCadastro.jsx";
+
+class App extends Component {
+  
+  render() {
+    return (
+      <section>
+            <FormularioCadastro />
+            <ListaDeNotas />
+      </section>
+    );
+  }
+
 }
 
 export default App;
